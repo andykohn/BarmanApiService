6b0bccf865ef31a0c91816a71f109197c48a35cc
Kohn <andykohn@amazon.com>
2017-08-04 16:43:07 -0700

    Continuing constructing ingredients. Still a problem that it overrides them

--- athena_barman.py
+++ athena_barman.py
@@ -104,6 +104,7 @@ class AthenaBarman:
 
     def __process_result_rows(self, client, query_execution_id):
 
+        return_results = {}
         query_results = self.__process_athena_results(client, query_execution_id)
         print query_results
 
@@ -113,12 +114,13 @@ class AthenaBarman:
 
             results = query_results['ResultSet']
 
-            for row in results:
+            for i, row in enumerate(results['Rows']):
                 if not is_first_row_processed:
                     # Process the row. The first row of the first page holds the column names.
+                    is_first_row_processed = True
                     continue
-                # processRow(row, columnInfoList)
-
+                item = self.__process_drink_row(row['Data'], i, column_info_list)
+                return_results.update(item)
             # If the nextToken is null, there are no more pages to read. Break out of the loop.
             next_token = query_results.get('NextToken', None)
             if next_token is None:
@@ -128,46 +130,32 @@ class AthenaBarman:
                 QueryExecutionId=query_execution_id,
                 NextToken=next_token
             )
-#
-# private static void processRow(Row row, List<ColumnInfo> columnInfoList)
-# {
-# for (int i = 0; i < columnInfoList.size(); ++i) {
-# switch (columnInfoList.get(i).getType()) {
-# case "varchar":
-# // Convert and Process as String
-# break;
-# case "tinyint":
-# // Convert and Process as tinyint
-# break;
-# case "smallint":
-# // Convert and Process as smallint
-# break;
-# case "integer":
-# // Convert and Process as integer
-# break;
-# case "bigint":
-# // Convert and Process as bigint
-# break;
-# case "double":
-# // Convert and Process as double
-# break;
-# case "boolean":
-# // Convert and Process as boolean
-# break;
-# case "date":
-# // Convert and Process as date
-# break;
-# case "timestamp":
-# // Convert and Process as timestamp
-# break;
-# default:
-# throw new RuntimeException("Unexpected Type is not expected" + columnInfoList.get(i).getType());
-# }
-# }
-# }
+
+    def __process_drink_row(self, row, ingredient_count, column_info_list):
+        drink = {'iddrink': 1, 'strdrink': 'drink name', 'strcategory': 'category',
+                 'strglass': 'drink name',
+                 'strinstructions': 'drink name',
+                 'ingredients':{'id':1, 'ingredientid': 'ingredient name', 'measurement':'1 oz'}}
+        item = {}
+        ingredients = {}
+        valid_columns_drink = ['iddrink', 'strdrink', 'strcategory', 'category', 'strglass', 'strinstructions']
+        valid_columns_ingredients = ['ingredients', 'id', 'ingredientid', 'measurement']
+
+        for i, column in enumerate(column_info_list):
+            column_name = column_info_list[i]['Name']
+            # Only for first row, since it's the drink itself
+            if not ingredient_count != 1:
+                if column_name in valid_columns_drink:
+                    item.update({column_name: row[i]})
+            # and here are the ingredients
+            if column_name in valid_columns_ingredients:
+                #item.update({'ingredients': {'id': ingredient_count, column_name: row[i]}})
+                item['ingredients'] = {'id': ingredient_count, column_name: row[i]}
+                #ingredients['ingredients'].update({column_name: row[i]})
 
 
-
+        #item = {id: {ingredients}}
+        return item
 
 
     def __my_logging_handler(self, event):
